68679b89e077c469d13c99a8935dc4e4b651e209
Berry  Semexan <bsemexan@gmail.com>
2018-02-27 04:06:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   def initialize(id=nil, name, grade)
     @id = id
     @name = name
-    @grade = grade  
+    @grade = grade
   end
 
   def self.create_table
@@ -44,7 +44,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = self.new
+    new_student = Student.new
     new_student.id = row[0]
     new_student.name= row[1]
     new_student.grade = row[2]
