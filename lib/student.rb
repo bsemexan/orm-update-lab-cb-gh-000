03547b24480fadca8ca8f11ea74033afc99eb80d
Berry  Semexan <bsemexan@gmail.com>
2018-02-27 03:40:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,4 +37,13 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
   end
 
+  def self.new_from_db(row)
+    # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name= row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
 end
