447819a29bdfc2a4ca02e4c0f8883ce5289edaaa
Berry  Semexan <bsemexan@gmail.com>
2018-02-27 03:56:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id=nil, grade, id=nil)
+  def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
     @id = id
