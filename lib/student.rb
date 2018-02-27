cc7cc012c44c484d18cc376888e9be63cef2fe31
Berry  Semexan <bsemexan@gmail.com>
2018-02-27 03:51:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,7 +34,7 @@ class Student
       self.update
     else
       sql = <<-SQL
-        INSERT INTO students (name, album)
+        INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.grade)
